a567cb0e242a596b4409e0753326ba5eece27618
Adrian Unger <dev@staydecent.ca>
2017-02-22 14:37:04 -0800

    Cleanup, prep for new iterations.

--- draw.js
+++ draw.js
@@ -1,50 +1,89 @@
-;(function drawLoop(){
-    requestAnimFrame(drawLoop)
-    draw()
-    update()
+/* globals THREE */
+
+// renderer
+var renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.getElementById('container').appendChild(renderer.domElement)
+
+// scene, plane
+var scene = new THREE.Scene()
+var imageData
+var imageReady = false
+var planeTexture = THREE.ImageUtils.loadTexture('gibson.jpg', new THREE.UVMapping(), function (e) {
+  imageData = window.getImageData(planeTexture.image)
+  imageReady = true
+})
+var planeMaterial = new THREE.MeshBasicMaterial({ map: planeTexture })
+var planeGeometry = new THREE.PlaneGeometry(592, 396)
+var plane = new THREE.Mesh(planeGeometry, planeMaterial)
+plane.position.z = -1
+
+// camera
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000)
+camera.position.set(0, -150, 600)
+camera.lookAt(plane.position)
+
+// lights
+var pointLight = new THREE.PointLight(0xcc00ff)
+pointLight.position.set(0, 150, 100)
+
+// lines!
+var lineMaterial = new THREE.LineBasicMaterial({color: 0x0000ff, linewidth: 5})
+
+// add to the scene!
+scene.add(camera)
+// scene.add(plane)
+scene.add(pointLight)
+
+// Render atleast once
+renderer.render(scene, camera)
+
+;(function drawLoop () {
+  window.requestAnimFrame(drawLoop)
+  draw()
+  update()
 })()
 
 var increaseZ = true
-var increaseY = true
 
 // use this for keyboard controls etc.
-function update() {
+function update () {
 }
 
 // draw yer shit
-function draw() {
-    // loop camera position
-    if (camera.position.z > 980) increaseZ = false
-    if (camera.position.z < 20)  increaseZ = true
+function draw () {
+  // loop camera position
+  if (camera.position.z > 980) increaseZ = false
+  if (camera.position.z < 20) increaseZ = true
 
-    if (camera.position.z < 1000 && increaseZ) {
-        camera.position.z = camera.position.z + 10
+  if (camera.position.z < 1000 && increaseZ) {
+    camera.position.z = camera.position.z + 5
+  }
+
+  if (camera.position.z > 0 && !increaseZ) {
+    camera.position.z = camera.position.z - 5
+  }
+
+  // draw lines when image is ready
+  if (imageReady) {
+    imageReady = false
+
+    // 1 line for every 10 pixels of image height
+    for (var y = 0; y <= imageData.height; y = y + 10) {
+      var lineGeometry = new THREE.Geometry()
+      lineGeometry.vertices = []
+
+      // 1 vertice for every 10 pixels of width
+      for (var x = 0; x <= imageData.width; x = x + 10) {
+        var colour = window.getPixel(imageData, x, y)
+        lineGeometry.vertices.push(new THREE.Vector3(x - (imageData.width / 2), y - (imageData.height / 2), colour.r / 2))
+      }
+
+      var line = new THREE.Line(lineGeometry, lineMaterial)
+      scene.add(line)
     }
+  }
 
-    if (camera.position.z > 0 && !increaseZ) {
-        camera.position.z = camera.position.z - 10
-    }
-
-    // draw lines when image is ready
-    if (imageReady) {
-        imageReady = false
-
-        // 1 line for every 10 pixels of image height
-        for (var y = 0; y <= imageData.height; y=y+10) {
-            var lineGeometry = new THREE.Geometry()
-            lineGeometry.vertices = []
-
-            // 1 vertice for every 10 pixels of width
-            for (var x = 0; x<=imageData.width; x=x+10) {
-                var colour = getPixel(imageData, x, y)
-                lineGeometry.vertices.push(new THREE.Vector3(x - (imageData.width / 2), y - (imageData.height / 2), colour.r))
-            }
-
-            var line = new THREE.Line(lineGeometry, lineMaterial)
-            scene.add(line)
-        }
-    }
-
-    // render
-    renderer.render(scene, camera)
+  // render
+  renderer.render(scene, camera)
 }
--- helpers.js
+++ helpers.js
@@ -1,15 +1,21 @@
-function getImageData(image) {
-    var canvas = document.createElement( 'canvas' );
-    canvas.width = image.width;
-    canvas.height = image.height;
+window.getImageData = function (image) {
+  var canvas = document.createElement('canvas')
+  canvas.width = image.width
+  canvas.height = image.height
 
-    var context = canvas.getContext( '2d' );
-    context.drawImage( image, 0, 0 );
+  var context = canvas.getContext('2d')
+  context.drawImage(image, 0, 0)
 
-    return context.getImageData( 0, 0, image.width, image.height );
+  return context.getImageData(0, 0, image.width, image.height)
 }
 
-function getPixel(imagedata, x, y) {
-    var position = ( x + imagedata.width * y ) * 4, data = imagedata.data;
-    return { r: data[ position ], g: data[ position + 1 ], b: data[ position + 2 ], a: data[ position + 3 ] };
+window.getPixel = function (imagedata, x, y) {
+  var position = (x + imagedata.width * y) * 4
+  var data = imagedata.data
+  return {
+    r: data[position],
+    g: data[position + 1],
+    b: data[position + 2],
+    a: data[position + 3]
+  }
 }
